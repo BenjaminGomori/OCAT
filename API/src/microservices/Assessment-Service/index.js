816e88cba4551e953ad34c9e342178e6cb8d5875
benjamin gomori <benjamingomori@gmail.com>
2021-03-08 16:03:48 -0500

    (for now) Making sure data is arriving at he microservices.

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -4,7 +4,10 @@ exports.submit = (assessment) => {
   return new Promise(async (resolve, reject) => { //eslint-disable-line
     try {
       //use the bookshelf model Assessments from API/src/microservices/Database to save the assessment data in the PostgreSQL database
+      console.log('before calling database')
+      console.log(assessment)
 
+      
       resolve();
     } catch (err) {
       reject(err);
