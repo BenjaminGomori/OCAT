65ceae5b0c6e865891190e0dc4ab630ec3338a4d
benjamin gomori <benjamingomori@gmail.com>
2021-03-08 12:26:40 -0500

    Assesment submit http call, AssesmentService

--- OCAT/client/resources/js/components/shared/services/assessment.service.js
+++ OCAT/client/resources/js/components/shared/services/assessment.service.js
@@ -4,7 +4,14 @@ export class AssessmentService {
     static async submit (assessment) {
         try {
             //Choose the correct method, url, and data to send in a request to the express OCAT/server/routes
-            await axios({});
+            //await axios({});
+            console.log('sending ');
+            console.log( assessment);
+
+            let res = await axios.post('http://127.0.0.1:3000/assessment/submit', assessment);
+
+            let data = res.data;
+            console.log(data);
 
             return;
         }
