11bb308c9070e2c3bd127d8178771f5621c46101
benjamin gomori <benjamingomori@gmail.com>
2021-03-08 23:12:20 -0500

    Set the database options to my database.

--- API/src/utils/Config/index.js
+++ API/src/utils/Config/index.js
@@ -18,9 +18,9 @@ module.exports = {
   database: {
     host: process.env.DATABASE_HOST || `localhost`,
     port: process.env.DATABASE_PORT || 5432,
-    username: process.env.DATABASE_USERNAME || `postgres`,
-    password: process.env.DATABASE_PASSWORD || `paul`,
-    name: process.env.DATABASE_NAME || `risk`,
+    username: process.env.DATABASE_USERNAME || `cat`,
+    password: process.env.DATABASE_PASSWORD || `1234`,
+    name: process.env.DATABASE_NAME || `OCAT`,
     dialect: process.env.DATABASE_DIALECT || `pg`,
     minconnections: process.env.DATABASE_MINCONNECTIONS || 2
   }
