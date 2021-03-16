801aa571a483ea8a4a9fc26eb1562acf1826c8fb
benjamin gomori <benjamingomori@gmail.com>
2021-03-15 20:12:39 -0400

    Correcting the ocat/router path - back to its initial state, '/api/assessment'.

--- OCAT/server/routes/AssessmentAPI/index.js
+++ OCAT/server/routes/AssessmentAPI/index.js
@@ -4,12 +4,8 @@ const { ErrorHandler } = require(`../../utils`);
 const { AssessmentSubmitRoute } = require(`../../utils`);
 
 router.post(`/submit`, (req, res) => {
-    //call the AssessmentSubmit function from the server/libs/AssessmentService
     AssessmentService.submit(req.body);
-    
 });
 
 exports.router = router;
-// TODO what is correct?
-// exports.path = `/api/assessment`;//original 
-exports.path = `/assessment`;
+exports.path = `/api/assessment`; 
