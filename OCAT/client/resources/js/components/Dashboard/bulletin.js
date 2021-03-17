aae981956b6f068aa260a4da0d4a6bbd027b9e38
benjamin gomori <benjamingomori@gmail.com>
2021-03-17 00:47:08 -0400

    Added NavLink to assessment/list and adjusted bootstrap styling.

--- OCAT/client/resources/js/components/Dashboard/bulletin.js
+++ OCAT/client/resources/js/components/Dashboard/bulletin.js
@@ -15,11 +15,20 @@ export class DashboardBulletin extends React.Component {
     return (
         <>
             <div className="container mt-3">
-              <div className="row justify-content-md-center">
+              <div className="justify-content-md-center">
                 <div className="col-12 col-md-8">
-                  <h1 >OCAT Dashboard</h1>
-                  <NavLink to="/assessment/new">New</NavLink>
-                  {/* <button href="/assessment/list">List</button> */}
+                  <div className="row">
+                    <h1 >OCAT Dashboard</h1>
+                  </div>
+                  <div className="row">
+                    <div className="col-auto">
+                      <NavLink to="/assessment/new">New</NavLink>
+                    </div>
+                    <div className="col-auto">
+                      <NavLink to="/assessment/list">List</NavLink>
+                      {/* <button href="/assessment/list">List</button> */}
+                    </div>
+                  </div>
                 </div>
               </div>
               <hr />
