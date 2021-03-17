16d8276beb392765728e7c62832c836e0bc8fe3e
benjamin gomori <benjamingomori@gmail.com>
2021-03-17 00:49:05 -0400

    Added retrieveAll() to ocat/ AssessmentService.

--- OCAT/client/resources/js/components/shared/services/assessment.service.js
+++ OCAT/client/resources/js/components/shared/services/assessment.service.js
@@ -11,4 +11,19 @@ export class AssessmentService {
             throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
         }
     }
+
+    static async retrieveAll() {
+        try {
+            //console.log('2222222222222222222222222222222222222');
+            const assessments =  axios.get('http://localhost:4567/api/assessment/retrieve').then((res) => {
+                console.log(res);
+                return res;
+             });
+            console.log(assessments);    
+            return assessments;
+        }
+        catch (err) {
+            throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
+        }
+    }
 }
