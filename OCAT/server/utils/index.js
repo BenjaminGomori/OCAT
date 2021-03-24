2f02213b927bfa9818d078485c02bf9043d28b43
benjamin gomori <benjamingomori@gmail.com>
2021-03-24 15:17:35 -0400

    OCAT - utils - Added LoginRoute to index file exports.

--- OCAT/server/utils/index.js
+++ OCAT/server/utils/index.js
@@ -1,9 +1,11 @@
 const ErrorHandler = require(`./ErrorHandler`);
 const IndexRoute = require(`./IndexRoute`);
 const AssessmentSubmitRoute = require(`./AssessmentSubmitRoute`);
+const LoginRoute = require(`./LoginRoute`);
 const RouteLoader = require(`./RouteLoader`);
 
 exports.ErrorHandler = ErrorHandler;
 exports.IndexRoute = IndexRoute;
 exports.AssessmentSubmitRoute = AssessmentSubmitRoute;
+exports.LoginRoute = LoginRoute;
 exports.RouteLoader = RouteLoader;
