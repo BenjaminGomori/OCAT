beafc819372ada5316e3365bb52baee26b386b0c
benjamin gomori <benjamingomori@gmail.com>
2021-03-24 15:11:15 -0400

    API - Added logic the routers\Users

--- API/src/routes/User/index.js
+++ API/src/routes/User/index.js
@@ -4,5 +4,25 @@ const { IdentityService } = require(`../../microservices`);
 const BASE_URL = `/user`;
 
 module.exports = server => {
-  
+
+    server.post(
+        `${ BASE_URL }/submit`,
+        async (req, res, next) => {
+          try {
+    
+            const  {login}  = req.params;
+            await IdentityService.createSession(login);
+    
+            ResponseHandler(
+              res,
+              'User Login Succeeded',
+              null,
+              next
+            );
+          } catch (err) {
+            console.log(err)
+            next(err);
+          }
+        }
+      );
 };
