47e539e6a357c55688bef070203670948909ab9c
benjamin gomori <benjamingomori@gmail.com>
2021-03-25 02:06:24 -0400

    Added logic to store in local storage.

--- OCAT/client/resources/js/components/shared/services/login.service.js
+++ OCAT/client/resources/js/components/shared/services/login.service.js
@@ -1,14 +1,30 @@
 import axios from "axios";
 import config from "../../../../../../config.json";
+// from https://loizenai.com/reactjs-jwt-authentication-example/#implement-reactjs-jwt-authentication-service
 
 export class LoginService {
     static async submit (login) {
         try {
-            await axios.post('http://localhost:4567/api/login/submit', login);
+            await axios.post('http://localhost:4567/api/login/submit', login).then(response => {
+                console.log(response);
+                if (response) {
+                  localStorage.setItem("user", JSON.stringify(response));
+                }
+                return response;
+              })
+              console.log('hmmmmm');
             return;
         }
         catch (err) {
             throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
         }
+    }
+
+    static signOut() {
+        localStorage.removeItem("user");
+    }
+
+    static getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));
     }
 }
