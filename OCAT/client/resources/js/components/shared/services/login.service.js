2f38447a0e9c8534b4e0bfd85ffa89f6b18b9ccf
benjamin gomori <benjamingomori@gmail.com>
2021-03-31 12:12:05 -0400

    login.service - added functions to provide components with user state currently stored in local storage, and for logging out.

--- OCAT/client/resources/js/components/shared/services/login.service.js
+++ OCAT/client/resources/js/components/shared/services/login.service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Redirect } from "react-router";
 import config from "../../../../../../config.json";
 // from https://loizenai.com/reactjs-jwt-authentication-example/#implement-reactjs-jwt-authentication-service
 
@@ -6,25 +7,33 @@ export class LoginService {
     static async submit (login) {
         try {
             await axios.post('http://localhost:4567/api/login/submit', login).then(response => {
-                console.log(response);
-                if (response) {
-                  localStorage.setItem("user", JSON.stringify(response));
-                }
-                return response;
+                localStorage.setItem("isSupervisor", JSON.stringify(response.data));
+                localStorage.setItem("isLogedIn", true);
+
+                window.location.replace("http://localhost:4567/assessment/list");
               })
-              console.log('hmmmmm');
-            return;
         }
         catch (err) {
             throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
         }
     }
 
-    static signOut() {
-        localStorage.removeItem("user");
+    static async logout() {
+        try {
+            localStorage.removeItem("isSupervisor");
+            localStorage.removeItem("isLogedIn");
+            //await axios.get('http://localhost:4567/api/logout');
+            window.location.replace("http://localhost:4567/api/logout");
+        }
+        catch (err) {
+            throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
+        }
     }
 
     static getCurrentUser() {
-        return JSON.parse(localStorage.getItem('user'));
+        const user = {};
+        user.isLogedIn = JSON.parse(localStorage.getItem('isLogedIn'));
+        user.isSupervisor = JSON.parse(localStorage.getItem('isSupervisor'));
+        return user;
     }
 }
