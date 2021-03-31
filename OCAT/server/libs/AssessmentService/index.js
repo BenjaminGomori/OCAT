49d56834872bc9673ac56c5326af8761ed84bd15
benjamin gomori <benjamingomori@gmail.com>
2021-03-31 18:24:10 -0400

    OCAT/libs- added delete logic.

--- OCAT/server/libs/AssessmentService/index.js
+++ OCAT/server/libs/AssessmentService/index.js
@@ -42,9 +42,7 @@ exports.submit = ( assessment ) => {
           },
           json: true
       };
-  
-      //this function sends a request to the API
-      // finish the logic to handle the response when returned from the API
+
       request(options, (error, response) => {
         if(error == null){
           resolve(response);
@@ -55,3 +53,32 @@ exports.submit = ( assessment ) => {
       });
     });
   };
+
+exports.delete = ( assessmentId) => {
+  return new Promise((resolve, reject) => {
+
+    //supply the correct uri and method here
+    const options = {
+        uri: `http://${ config.api.url}/assessment/delete/`,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: {
+          assessmentId: assessmentId
+        },
+        json: true
+    };
+
+    //this function sends a request to the API
+    // finish the logic to handle the response when returned from the API
+    request(options, (error, response) => {
+      if(error == null){
+        resolve(response);
+      }
+      if(error != null){
+        reject(error);
+      }
+    });
+  });
+};
