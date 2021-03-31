cbbc95b821a9950af64d04b5532ca7d4ba51f2ac
benjamin gomori <benjamingomori@gmail.com>
2021-03-31 18:20:46 -0400

    Microservices - Added delete logic.

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -16,12 +16,12 @@ exports.submit = (assessment) => {
         'deleted_at': null
       }
 
-      let newAssessment = await new Assessments(val).save().catch(function (e) {
+      await new Assessments(val).save()
+      then(()=>{resolve()})
+      .catch(function (err) {
         console.log('error in saving assessment')
-        resolve(newAssessment);
+        reject();
       });
-
-      resolve(newAssessment);
 
     } catch (err) {
       reject();
@@ -32,12 +32,40 @@ exports.submit = (assessment) => {
 exports.retrieve = () => {
   return new Promise(async (resolve, reject) => { //eslint-disable-line
     try {
-      let assesmentList = await new Assessments().where({deleted_at: null}).fetchAll().catch(function (e) {
-        console.log('error in retrieving assessments')
-        resolve();
+        await new Assessments().fetchAll()
+        .then((assesmentList)=>{resolve(assesmentList)})
+        .catch(function (err) {
+          console.log('error in retrieving assessments')
+          resolve();
       });
+    } catch (err) {
+      reject();
+    }
+  });
+};
 
-      resolve(assesmentList.toJSON());
+exports.delete = (assessmentId) => {
+  return new Promise(async (resolve, reject) => { //eslint-disable-line
+    try {
+      const date = new Date();
+      const shortDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+      //from https://stackoverflow.com/questions/41914802/how-do-i-properly-update-models-with-bookshelf-js
+      await new Assessments({'id':assessmentId}).fetch()
+      .then(function (model) {
+        if (model) {
+          var params = { 'deleted_at': shortDate }
+           model.save(params, {
+            method: 'update',
+            patch: true
+          });
+        } 
+      })
+      .then(()=>{resolve()})
+      .catch(function (err) {
+          console.log("error in deleting assessment", err);
+          resolve();
+      });
 
     } catch (err) {
       reject();
