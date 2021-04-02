c2ab9132a852869afdacf84f9686ef9900e64e13
benjamin gomori <benjamingomori@gmail.com>
2021-04-02 00:15:00 -0400

    API, Password - Added logic to provide new hashed passwords to enable new user sign up. Was not able to figure out how to return the hashed password without using a callback function.

--- API/src/utils/Password/index.js
+++ API/src/utils/Password/index.js
@@ -1,9 +1,32 @@
+const { resolve } = require('app-root-path');
 const bcrypt = require ('bcrypt');
 
-  const copmarePasswords = async function (password, storedPassword, callBack ) {
-    await bcrypt.compare(password, storedPassword, function(err, result) {
-        callBack(result);
-    });;
+// const copmarePasswords = async function (password, storedPassword, callBack ) {
+  //const copmarePasswords = async function (password, storedPassword) {
+    // let isSamePassword = await bcrypt.compare(password, storedPassword, function(err, result) {
+    //   console.log('result');
+    //   console.log(result);
+    //   resolve(''+result);
+    //   return result;
+  // });
+  // console.log(isSamePassword);
+  // return isSamePassword;
+// }
+
+const copmarePasswords = async function (password, storedPassword, callBack ) {
+  await bcrypt.compare(password, storedPassword, function(err, result) {
+      callBack(result);
+  });;
 }
 
-module.exports = copmarePasswords;
+const getEncryptedPassword = async function (regularPassword, callback) {
+  let hashedPassword = await bcrypt.hash(regularPassword, 10).then(function(hash) {
+    console.log(hash);
+    return hash;
+  });
+  console.log('2nd',hashedPassword);
+  return hashedPassword;
+}
+
+module.exports.comparePassword = copmarePasswords;
+module.exports.getEncryptedPassword = getEncryptedPassword;
