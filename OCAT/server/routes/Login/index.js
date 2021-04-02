6159e909e5667101064dae0ec9029894e6c2ac81
benjamin gomori <benjamingomori@gmail.com>
2021-04-02 00:24:03 -0400

    OCAT , Login route -  added sin up logic.  And changed the session logic for signing in.

--- OCAT/server/routes/Login/index.js
+++ OCAT/server/routes/Login/index.js
@@ -4,21 +4,25 @@ const { UserService } = require(`../../libs`);
 router.post(`/submit`, (req, res) => {
 
     const sess = req.session;
-    const { username, password } = req.body;
-    sess.username = username;
-    sess.password = password;
-    sess.isSupervisor = false;
-    sess.isLoggedIn = false;
+    console.log(req.body);
 
     UserService.submit(req.body).then((response) => {
-        sess.isLoggedIn = true;
-        if(!response.body.data.username){
-            sess.isSupervisor = false;
-        }else{
-            sess.isSupervisor = true;
-        }
+        console.log('back in Frone End Router',response.body.data );
+        sess.isLoggedIn = response.body.data.isLoggedIn;
+        sess.isSupervisor = response.body.data.is_supervisor;
+        sess.username = response.body.data.username;
 
-        res.send(sess.isSupervisor);
+        console.log('seesion',sess );
+
+        res.send({isLoggedIn: response.body.data.isLoggedIn, isSupervisor: response.body.data.is_supervisor});
+    });
+});
+
+router.post(`/signUp`, (req, res) => {
+    UserService.signUp(req.body).then((response) => {
+        console.log('222222222222222222222');
+        console.log(response.body.data);
+        res.send(response.body.data);
     });
 });
 
