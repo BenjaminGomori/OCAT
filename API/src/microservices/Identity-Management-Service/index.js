7efa4ef3324d2cba9b7b645afcf32f6e97bfa58c
benjamin gomori <benjamingomori@gmail.com>
2021-04-02 00:11:31 -0400

    Adjusting microservices logic to allow dyncamic signUp.

--- API/src/microservices/Identity-Management-Service/index.js
+++ API/src/microservices/Identity-Management-Service/index.js
@@ -1,41 +1,106 @@
 const { Users } = require(`../Database`);
 const { Password } = require(`../../utils`);
 
-exports.supervisorLogin = (login) => {
+exports.login = (login) => {
   return new Promise(async (resolve, reject) => { //eslint-disable-line
     try {
 
-      let storedSupervisor = await new Users().where({id: 1}).fetch().catch(function (e) {
+     /// console.log('login ', login);
+      ///console.log('login.username ', login.username);
+      let user = await new Users().where({username: login.username}).fetch().catch(function (e) {
         console.log('error in retrieving supervisor information')
-        resolve();
+        resolve(createBadAttemptUser());
       });
 
-      const supervisor = storedSupervisor.toJSON()
-      let correctUsername = false;
+      //no user with that username
+      if(!user) resolve(createBadAttemptUser());
+      const supervisor = user.toJSON();
+      ///console.log('supervisor', supervisor);
 
-      correctUsername = validateUsername(login.username, supervisor);
-      if (correctUsername) {
-        validateSupervisorPassword(login.password, supervisor,returnResult);
-      }else{
-        resolve(false);
+      await validatePassword(login.password, supervisor,onPasswordsCompare.bind(this, supervisor));
+      
+
+
+      function onPasswordsCompare(user, isSame){
+        //console.log('isSame');
+        //console.log(isSame);
+
+        if(!isSame ) resolve(createBadAttemptUser());
+       // console.log('user!!!!!!!!!1');
+        //user = {...user};
+        user.isLoggedIn = true;
+        //console.log(user);
+
+        resolve (user);
       }
-
-      function returnResult(result){
-        resolve(result);
-      }
-
-      return;
 
     } catch (err) {
       reject();
     }
   });
 
-  function validateUsername(username, supervisor){
-    return username.trim() == supervisor.username;
+
+  async function validatePassword(password, supervisor, callBack){
+    await Password.comparePassword(password.trim(),supervisor.password, callBack);
   }
 
-  async function validateSupervisorPassword(password, supervisor, returnResult ){
-    await Password(password.trim(),supervisor.password, returnResult);
+  function createBadAttemptUser(){
+    user ={};
+    user.isLoggedIn = false;
+    user.is_supervisor = false;
+    user.username = '';
+    return user;
+  }
+
+};
+
+exports.signUp = (login) => {
+  return new Promise(async (resolve, reject) => { //eslint-disable-line
+    try {
+
+      const isUserNew = await isUserNameNew(login.username);
+      console.log('USER NEW!!!!!!!!!!!!   ==>>>',isUserNew)
+
+      if(isUserNew){
+        let user = await createUser(login);
+
+        const newUser = await new Users(user).save().catch(function (e) {
+          console.log('error in retrieving supervisor information', e)
+          resolve('false');
+        });
+      
+        resolve('true');
+    }
+
+    resolve('false');
+
+    } catch (err) {
+      reject();
+    }
+  });
+
+  async function createUser(login){
+    let password = await getEncryptedPassword(login.password);
+      let user = { 
+        'first_name': '',
+        'last_name':'',
+        'username': login.username,
+        'password': password,
+        'is_supervisor': login.isSupervisor,
+      }
+      return user
+  }
+
+  async function getEncryptedPassword(password){
+    let encryptedPassword = await Password.getEncryptedPassword(password);
+    return encryptedPassword;
+  }
+
+  async function isUserNameNew(username){
+    let user = await new Users().where({username: username}).fetch().catch(function (e) {
+      console.log('error in retrieving user information',e)
+      resolve();
+    });
+    return user?  false: true;
   }
 };
