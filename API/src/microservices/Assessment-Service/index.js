1bfcad2950704e1dd13d3fb9dcf35e94f529bf3e
benjamin gomori <benjamingomori@gmail.com>
2021-04-05 14:19:52 -0400

    Microservices - Added delete logic.

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -44,3 +44,32 @@ exports.retrieve = () => {
     }
   });
 };
+
+exports.delete = (assessmentId) => {
+  return new Promise(async (resolve, reject) => { //eslint-disable-line
+    try {
+      const date = new Date();
+      const shortDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+      //from https://stackoverflow.com/questions/41914802/how-do-i-properly-update-models-with-bookshelf-js
+      await new Assessments({'id':assessmentId}).fetch()
+      .then(function (model) {
+        if (model) {
+          var params = { 'deleted_at': shortDate }
+           model.save(params, {
+            method: 'update',
+            patch: true
+          });
+        } 
+      })
+      .then(()=>{resolve()})
+      .catch(function (err) {
+          console.log("error in deleting assessment", err);
+          resolve();
+      });
+
+    } catch (err) {
+      reject();
+    }
+  });
+};
